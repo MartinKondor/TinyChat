fa1b827791787c86eaf942b0ee66b127246c2dbf
Martin Kondor <martinkondor@gmail.com>
2020-01-24 11:16:18 +0100

    Remove unnecessary processing

--- eth.py
+++ eth.py
@@ -15,6 +15,7 @@ class ETHConnection:
             self.ETH.send(self._eth_encode(s))
         except Exception as e:
             print('Error when sending message ({}):{}\n'.format(s, str(e)))
+            self.reconnect()
 
     def recv(self, n):
         return self._eth_decode(self.ETH.recv(n))
@@ -35,7 +36,6 @@ class ETHConnection:
 
     def reconnect(self):
         self.close()
-        self.ETH = None
         self.connect()
 
     def close(self):
@@ -45,61 +45,12 @@ class ETHConnection:
             pass
 
     def _eth_decode(self, s):
-        """
-        Startsign: ~
-        Endsign: ;
-        """
-        new_s = ''
-        ns = s.decode('utf-8')
-        can_read = False
-
-        for ch in ns:
-            if ch == '~':
-                can_read = True
-                continue
-
-            if can_read and ch == ';':
-                break
-            elif can_read:
-                new_s += ch
-        else:
-            return ''
-
-        return new_s
+        return s.decode('utf-8')
 
 
     def _eth_encode(self, s):
-        return bytearray(('~' + str(s) + (';' if s[-1] != ';' else '')).encode('utf-8'))
+        return bytearray(str(s).encode('utf-8'))
             
 
 if __name__ == '__main__':
-    """
-    from threading import Thread
-
-
-    def send_msg():
-        while True:
-            ETH.send(eth_encode(input('>> ')))
-
-            
-    def get_msg():
-        while True:
-            data = eth_decode(ETH.recv(1024))
-            if data:
-                print(data)
-
-    ETH = eth_setup(IS_SERVER)
-    
-    try:
-        Thread(target=send_msg).start()
-        Thread(target=get_msg).start()
-        while True:
-            pass
-    except:
-        print('\nError happened:')
-        print(str(e))
-        print('\n')
-    finally:
-        ETH.close()
-    """
     pass
